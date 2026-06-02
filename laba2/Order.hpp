cb27b58e294d7af933e31c3bc65ed9061344fab9
dalnaevaalina-web <dalnaevaalina@gmail.com>
2026-06-02 06:46:47 +0300

    Delete laba2/Order.hpp

--- laba2/Order.hpp
+++
@@ -1,8 +0,0 @@
-struct Order {
-    int id;
-    int priority; // чем меньше тем важнее
-
-    bool operator<(const Order& other) const {
-        return priority > other.priority; // min priority сверху
-    }
-};
