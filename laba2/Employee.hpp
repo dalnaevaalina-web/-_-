9885fbdbc576fd1a199cac8132e15ba0731ddb8c
dalnaevaalina-web <dalnaevaalina@gmail.com>
2026-06-02 06:46:39 +0300

    Delete laba2/Employee.hpp

--- laba2/Employee.hpp
+++
@@ -1,8 +0,0 @@
-struct Employee {
-    int id;
-    bool high_priority;
-
-    bool operator<(const Employee& other) const {
-        return high_priority < other.high_priority; // VIP вперед
-    }
-};
